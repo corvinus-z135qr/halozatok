afefbddf284589e5a4c995bf2084dadfafac3cd9
corvinus-z135qr <alex.bodnar2@stud.uni-corvinus.hu>
2021-03-18 17:15:22 +0100

    6. gyakorlat (jsgyak.html / jsgyak.js) megoldása !

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,28 @@
+﻿window.onload = function () {
+
+    let hova = document.getElementById("ide")
+    hova.innerHTML = ""
+    let szam = 1;
+
+    for (var s = 0; s < 9; s++) {
+
+        let seged = s + 1;
+        let sor = document.createElement("div");
+        hova.appendChild(sor)
+        sor.classList.add("egymas_melle");
+
+        for (var o = 0; o <= s; o++) {
+            if (o > 0) {
+
+                let szám = document.createElement("div")
+                sor.appendChild(szám)
+                szám.innerText = szam * (seged - o) * o;
+                szám.classList.add("doboz");
+                szám.style.background = `rgb(${60 * 12 / s},0,${110 * 5 * o})`;
+            }
+
+
+
+        }
+    }
+}
